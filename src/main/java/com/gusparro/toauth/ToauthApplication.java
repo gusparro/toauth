ad41f02faf93b89bf631dde6b2bfb383a64ddf5e
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-03-11 21:15:31 -0300

    Criando o projeto.

---
+++ src/main/java/com/gusparro/toauth/ToauthApplication.java
@@ -0,0 +1,13 @@
+package com.gusparro.toauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gusparro/toauth/ToauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusparro.toauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
