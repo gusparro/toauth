3c8233d53fddbac1a352c0b365ec058ff5c2c126
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-03-15 12:07:15 -0300

    Adicionando endpoints na classe AppUserController.

--- src/main/java/com/gusparro/toauth/api/controllers/AppUserController.java
+++ src/main/java/com/gusparro/toauth/api/controllers/AppUserController.java
@@ -2,11 +2,15 @@ package com.gusparro.toauth.api.controllers;
 
 import com.gusparro.toauth.domain.entities.AppUser;
 import com.gusparro.toauth.domain.repositories.AppUserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/users")
@@ -21,6 +25,38 @@ public class AppUserController {
     @GetMapping
     public List<AppUser> getAll() {
         return appUserRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppUser> getById(@PathVariable Long id) {
+        return appUserRepository.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public AppUser persist(@RequestBody AppUser appUser) {
+        return appUserRepository.save(appUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AppUser> update(@PathVariable Long id, @RequestBody AppUser receivedAppUser) {
+        return appUserRepository.findById(id).map(appUser -> {
+            BeanUtils.copyProperties(receivedAppUser, appUser, "id");
+            appUserRepository.save(appUser);
+            return ResponseEntity.ok(appUser);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> remove(@PathVariable Long id) {
+        try {
+            return appUserRepository.findById(id).map(appUser -> {
+                appUserRepository.deleteById(id);
+                return ResponseEntity.noContent().build();
+            }).orElse(ResponseEntity.notFound().build());
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(CONFLICT).build();
+        }
     }
 
 }
