00b190868e5c4dad63564d5dde9733f125ece773
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-03-17 16:13:02 -0300

    Adicionando a dependência do Spring Security ao projeto.

---
+++ src/main/java/com/gusparro/toauth/api/controllers/configs/security/ApplicationSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.gusparro.toauth.api.controllers.configs.security;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+}
