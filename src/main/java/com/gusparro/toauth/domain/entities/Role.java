6e036f55d0a7ed27e389605bfb41e9275ede855e
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-03-18 14:16:11 -0300

    Criando a classe Role e mapeando o relacionamento com a classe AppUser.

--- src/main/java/com/gusparro/toauth/domain/entities/Role.java
+++ src/main/java/com/gusparro/toauth/domain/entities/Role.java
@@ -1,14 +1,11 @@
 package com.gusparro.toauth.domain.entities;
 
-
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 import java.util.Objects;
 
-import static javax.persistence.FetchType.EAGER;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Getter
@@ -17,33 +14,27 @@ import static javax.persistence.GenerationType.IDENTITY;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class AppUser {
+public class Role {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    private String fullName;
+    @Column(unique = true)
+    private String name;
 
-    private String email;
-
-    private String username;
-
-    private String password;
+    private String description;
 
     @ToString.Exclude
-    @ManyToMany(fetch = EAGER)
-    @JoinTable(name = "appuser_role",
-            joinColumns = @JoinColumn(name = "appuser_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private List<Role> roles;
+    @ManyToMany(mappedBy = "roles")
+    private List<AppUser> appUsers;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        AppUser appUser = (AppUser) o;
-        return id.equals(appUser.id);
+        Role role = (Role) o;
+        return id.equals(role.id);
     }
 
     @Override
