ac74d1fe2bc5673d49c4c8dd3fca81dabdfa26f8
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-04-06 10:50:39 -0300

    Mudando o nome da migração V004__insert-into-appuser-role-appuser_role para afterMigrate, seguindo as boas praticas do Flyway.

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,46 @@
+INSERT INTO appuser (id, full_name, email, username, password)
+VALUES (1, 'Gustavo Ferreira Parro', 'gustavo.ferreira.parro@gmail.com', 'gusparro',
+        '$2a$10$I0Ti.ZrqTA02wuCzStTmK.ywtZSZ1a76FGGUqlQzJA8lwMZoQgN36')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser (id, full_name, email, username, password)
+VALUES (2, 'Iann Carlo', 'iannCarlo@gmail.com', 'iann',
+        '$2a$10$I0Ti.ZrqTA02wuCzStTmK.ywtZSZ1a76FGGUqlQzJA8lwMZoQgN36')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser (id, full_name, email, username, password)
+VALUES (3, 'Wendel Silva', 'wendel_silva@gmail.com', 'wendelzin',
+        '$2a$10$I0Ti.ZrqTA02wuCzStTmK.ywtZSZ1a76FGGUqlQzJA8lwMZoQgN36')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO role (id, name, description)
+VALUES (1, 'DRIVER_ROLE', 'Profile created for use by company drivers.')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO role (id, name, description)
+VALUES (2, 'MANAGER_ROLE', 'Profile created for use by company managers.')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO role (id, name, description)
+VALUES (3, 'ADMINISTRATOR_ROLE', 'Profile created for use by company administrators.')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser_role (id, appuser_id, role_id)
+VALUES (1, 1, 1)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser_role (id, appuser_id, role_id)
+VALUES (2, 1, 2)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser_role (id, appuser_id, role_id)
+VALUES (3, 1, 3)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser_role (id, appuser_id, role_id)
+VALUES (4, 2, 2)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO appuser_role (id, appuser_id, role_id)
+VALUES (5, 3, 1)
+ON CONFLICT DO NOTHING;
