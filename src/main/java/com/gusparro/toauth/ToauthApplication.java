af74074c2c9d1fd5ea49a0b32e6ddaf3ae947fd7
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-04-19 13:02:10 -0300

    Ajustando a aplicação para utilizar UTC nas datas.

--- src/main/java/com/gusparro/toauth/ToauthApplication.java
+++ src/main/java/com/gusparro/toauth/ToauthApplication.java
@@ -3,10 +3,14 @@ package com.gusparro.toauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ToauthApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        
         SpringApplication.run(ToauthApplication.class, args);
     }
 
