4d95f865423a04e4dd42cb88fa1df8eabb359567
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-06-07 17:34:42 -0300

    Criando a classe AppUserSpecifications.

--- src/main/java/com/gusparro/toauth/domain/repositories/AppUserRepository.java
+++ src/main/java/com/gusparro/toauth/domain/repositories/AppUserRepository.java
@@ -2,12 +2,13 @@ package com.gusparro.toauth.domain.repositories;
 
 import com.gusparro.toauth.domain.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+public interface AppUserRepository extends JpaRepository<AppUser, Long>, JpaSpecificationExecutor<AppUser> {
 
     Optional<AppUser> findByUsername(String username);
 
---
+++ src/main/java/com/gusparro/toauth/domain/repositories/specifications/AppUserSpecifications.java
@@ -0,0 +1,10 @@
+package com.gusparro.toauth.domain.repositories.specifications;
+
+import com.gusparro.toauth.domain.entities.AppUser;
+import org.springframework.data.jpa.domain.Specification;
+
+public class AppUserSpecifications {
+
+    
+
+}
