40b8dc56cc75313957fa5c9ff90c18e6a4b6e4a1
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-06-14 17:25:37 -0300

    Adicionando Metamodel do JPA para as especificações.

--- src/main/java/com/gusparro/toauth/domain/repositories/specifications/AppUserSpecifications.java
+++ src/main/java/com/gusparro/toauth/domain/repositories/specifications/AppUserSpecifications.java
@@ -2,6 +2,7 @@ package com.gusparro.toauth.domain.repositories.specifications;
 
 import com.gusparro.toauth.api.dtos.appuser.AppUserSearchFilter;
 import com.gusparro.toauth.domain.entities.AppUser;
+import com.gusparro.toauth.domain.entities.AppUser_;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.util.StringUtils;
@@ -16,15 +17,15 @@ public class AppUserSpecifications {
             List<Predicate> predicates = new ArrayList<>();
 
             if (StringUtils.hasLength(searchFilter.fullName())) {
-                predicates.add(builder.like(builder.upper(root.get("fullName")), "%" + searchFilter.fullName().toUpperCase() + "%"));
+                predicates.add(builder.like(builder.upper(root.get(AppUser_.fullName)), "%" + searchFilter.fullName().toUpperCase() + "%"));
             }
 
             if (StringUtils.hasLength(searchFilter.email())) {
-                predicates.add(builder.like(builder.upper(root.get("email")), "%" + searchFilter.email().toUpperCase() + "%"));
+                predicates.add(builder.like(builder.upper(root.get(AppUser_.email)), "%" + searchFilter.email().toUpperCase() + "%"));
             }
 
             if (StringUtils.hasLength(searchFilter.username())) {
-                predicates.add(builder.like(builder.upper(root.get("username")), "%" + searchFilter.username().toUpperCase() + "%"));
+                predicates.add(builder.like(builder.upper(root.get(AppUser_.username)), "%" + searchFilter.username().toUpperCase() + "%"));
             }
 
             return builder.and(predicates.toArray(new Predicate[0]));
